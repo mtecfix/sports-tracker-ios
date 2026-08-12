0b2d87e46ef30a2731451e9888783a7850f6518b
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-11 21:46:17 -0400

    Add source files

---
+++ SportsApp/Config.swift
@@ -0,0 +1,6 @@
+import Foundation
+enum Config {
+    static let apiEndpoint   = "https://i2i5aq78u1.execute-api.us-east-1.amazonaws.com/dev"
+    static let cognitoPoolId = "us-east-1_9LQYVOUIJ"
+    static let cognitoClient = "o7rpn3nvfe3iu2hm0e2li1qdk"
+}
